5a54da3a3288710cf2432ce0cdd8883725a9c079
Ananya-Mittal <ananyamittal0905@gmail.com>
2024-09-27 21:13:51 +0530

    User login pages

---
+++ requirementsPg.js
@@ -0,0 +1,8 @@
+document.getElementById('submitBtn').addEventListener('click', function() {
+    const requirement = document.getElementById('requirement').value;
+    const type = document.getElementById('type').value;
+    const specialist = document.getElementById('specialist').value;
+    const hospital = document.getElementById('hospital').value;
+
+   
+});
---
+++ services.js
@@ -0,0 +1,36 @@
+ // JavaScript function to open a new page for Bed Availability
+ function openBedAvailability() {
+    // Replace with the actual URL of the Bed Availability page
+    window.open('http://127.0.0.1:5500/Medix/requirementPg.html', '_blank');
+    // app.js
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const patients = [{
+    name: 'Aditya',
+    number: '8175826846',
+    dob: '29/09/2001',
+    city: 'Mirzapur',
+    roomNo: '1',
+}]
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.get("/", function (req, res) {
+    res.render("home", {
+        data: patients
+    })
+})
+
+app.listen(3000, (req, res) => {
+    console.log("App is running on port 3000")
+})
+
+}
